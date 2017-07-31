b4db20c1b03cdf053e0fb3a9494a5528421317db
Crazycolorz5 <crazycolorz5@gmail.com>
2017-07-31 16:32:21 -0400

    Removed most Blitz-relevant files and cleaned out Blitz content.

--- MAKE HACK_quick.cmd
+++ MAKE HACK_quick.cmd
@@ -1,9 +1,9 @@
 ﻿cd %~dp0
 
-copy FE8_clean.gba CCB_Round1.gba
+copy FE8_clean.gba FEU_Predestination.gba
 
 cd "%~dp0Event Assembler"
 
-Core A FE8 "-output:%~dp0CCB_Round1.gba" "-input:%~dp0ROM Buildfile.event"
+Core A FE8 "-output:%~dp0FEU_Predestination.gba" "-input:%~dp0ROM Buildfile.event"
 
 pause
